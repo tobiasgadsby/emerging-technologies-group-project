245baadaf16b0d26093e99476cf4cc5eabf28390
James Crake-Merani <james@jamescm.co.uk>
2026-02-26 18:02:07 +0000

    Initial commit.

---
+++ resource-mapping/test/jamescrake_merani/resource_mapping_test.clj
@@ -0,0 +1,7 @@
+(ns jamescrake-merani.resource-mapping-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [jamescrake-merani.resource-mapping :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
