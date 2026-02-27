0efd6ae7bae0738e1f4563ebb2b8446dee450c8b
James Crake-Merani <james@jamescm.co.uk>
2026-02-27 15:40:43 +0000

    Import example road map.

--- resource-mapping/src/jamescrake_merani/resource_mapping.clj
+++ resource-mapping/src/jamescrake_merani/resource_mapping.clj
@@ -1,3 +1,16 @@
 (ns jamescrake-merani.resource-mapping
+  (:require [loom.graph :as lg])
   (:gen-class))
 
+(def example-road-map
+  (lg/weighted-graph 
+    {:a {:b 14 :d 12 :e 13}
+     :b {:a 14 :c 16 :e 15 :f 12}
+     :c {:b 16 :f 20 :g 12}
+     :d {:a 12 :e 5  :h 12}
+     :e {:a 13 :b 15 :d 5  :f 9}
+     :f {:b 12 :c 20 :e 9  :g 16 :i 12}
+     :g {:c 12 :f 16 :i 20 :j 12}
+     :h {:d 12 :i 14}
+     :i {:f 12 :g 20 :h 14 :j 16}
+     :j {:g 12 :i 16}}))
