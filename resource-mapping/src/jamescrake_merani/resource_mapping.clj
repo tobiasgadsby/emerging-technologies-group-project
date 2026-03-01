a235436bb24c52ac193196628c3c76ab1ef42abb
James Crake-Merani <james@jamescm.co.uk>
2026-03-01 11:29:16 +0000

    Fixed typo.

--- resource-mapping/src/jamescrake_merani/resource_mapping.clj
+++ resource-mapping/src/jamescrake_merani/resource_mapping.clj
@@ -25,7 +25,7 @@
   {:hospital [(Coordinate. :b nil 0)
               (Coordinate. :i nil 0 )]
    :ambulance [(Coordinate. :d :e 2)
-               (Coordinate :g nil 0)]})
+               (Coordinate. :g nil 0)]})
 
 (defn nearest [graph current-position items]
   (apply min-key second (map (partial la/dijkstra-path-dist graph) (repeat (count items) current-position) items)))
