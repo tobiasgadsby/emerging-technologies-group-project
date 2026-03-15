6502127c465010b6fe304b0cfb910377d78f6e71
James Crake-Merani <james@Jamess-MacBook-Pro.local>
2026-03-15 20:48:20 +0000

    Get the nearest available ambulance.

--- resource-mapping/src/jamescrake_merani/resource_mapping.clj
+++ resource-mapping/src/jamescrake_merani/resource_mapping.clj
@@ -34,7 +34,8 @@
 ;; TODO: At the moment, just consider where the ambulance is, not where its going to.
 (defn dispatch [graph patient-location]
   (let [nearest-hospital (nearest graph patient-location (map :current-node (:hospital positions)))
-        nearest-ambulance (nearest graph patient-location (map :current-node (:ambulance positions)))]
+        available-ambulances (filter #(= (:status %) :random-walk) (:ambulance positions))
+        nearest-ambulance (nearest graph patient-location (map #(-> % :coordinate :current-node) available-ambulances))]
     {:eta (+ (second nearest-hospital) (second nearest-ambulance))
      :nearest-ambulance (last (first nearest-ambulance))
      :nearest-hospital (last (first nearest-hospital))}))
