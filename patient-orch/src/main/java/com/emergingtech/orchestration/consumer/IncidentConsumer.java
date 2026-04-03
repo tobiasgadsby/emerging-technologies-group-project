b5345afe99ff786d845e3f90932ee306d66dde84
Toby <133044093+tobiasgadsby@users.noreply.github.com>
2026-04-03 17:20:55 +0100

    orchestration - created initial api and proto specifications for orchestration, and implemented initial version of doctor request orchestration service allowing for the creation of incidents

--- patient-orch/src/main/java/com/emergingtech/orchestration/consumer/IncidentConsumer.java
+++ patient-orch/src/main/java/com/emergingtech/orchestration/consumer/IncidentConsumer.java
@@ -1,9 +1,13 @@
 package com.emergingtech.orchestration.consumer;
 
+import com.emergingtech.orchestration.db.model.Incident;
 import com.emergingtech.orchestration.db.service.IncidentDbService;
 import com.emergingtech.orchestration.mapper.IncidentMapper;
+import com.emergingtech.orchestration.producer.PatientProducer;
+import com.emergingtech.proto.Common.IncidentStatus;
 import com.emergingtech.proto.Incident.IncidentRequest;
 import com.emergingtech.proto.Incident.IncidentResponse;
+import com.emergingtech.proto.Patient.PatientUpdate;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -20,16 +24,26 @@ public class IncidentConsumer {
     @Inject
     IncidentMapper incidentMapper;
 
+    @Inject
+    PatientProducer patientProducer;
+
     @Incoming("incident-request")
     public void incidentRequest(IncidentRequest incident) {
-        LOG.infof("Incident Request Recieved, Patient ID: %d", incident.getPatientId());
+        LOG.infof("Incident Request Received, Patient ID: %d", incident.getPatientId());
         incidentDbService.createIncident(incidentMapper.mapIncidentRequestToIncidentEntity(incident));
     }
 
     @Incoming("incident-response")
     public void incidentResponse(IncidentResponse incident) {
         LOG.infof("Incident Response Received, Incident ID: %d", incident.getIncidentId());
-
+        incidentDbService.updateIncident(Incident.builder()
+                        .incidentId(incident.getIncidentId())
+                        .status(incident.getIncidentStatus().name())
+                .build());
+        patientProducer.updatePatient(PatientUpdate.newBuilder()
+                        .setPatientId(incident.getPatientId())
+                        .setIncidentStatus(incident.getIncidentStatus())
+                .build());
     }
 
 }
