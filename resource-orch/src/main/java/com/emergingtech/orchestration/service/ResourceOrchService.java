b3685e2df861e0c271b95fc37a98ce4122e032a9
James Crake-Merani <james@jamescm.co.uk>
2026-04-13 11:38:33 +0100

    Toby's magic fixed it.

--- resource-orch/src/main/java/com/emergingtech/orchestration/service/ResourceOrchService.java
+++ resource-orch/src/main/java/com/emergingtech/orchestration/service/ResourceOrchService.java
@@ -6,6 +6,7 @@ import com.emergingtech.orchestration.db.model.ResourceMapping;
 import com.emergingtech.orchestration.db.service.IncidentDbService;
 import com.emergingtech.orchestration.db.service.ResourceMappingDbService;
 import com.emergingtech.orchestration.producer.ResourceOrchProducer;
+import com.emergingtech.proto.Common;
 import com.emergingtech.proto.Common.Coordinates;
 import com.emergingtech.proto.Incident.IncidentResponse;
 import com.emergingtech.proto.ResourceMapper.PractitionerActionRequest;
@@ -59,7 +60,7 @@ public class ResourceOrchService {
         resourceMapping.setStatus(ResourceStatus.MAPPED.toString());
         resourceMapping.setEstimatedArrivalTime(Instant.ofEpochSecond(resourceMappingResponse.getArrivalTime().getSeconds(), resourceMapping.getEstimatedArrivalTime().getNano()).atZone(ZoneOffset.UTC).toLocalDateTime());
         resourceMappingDbService.updateResourceMapping(resourceMapping);
-        resourceOrchProducer.sendIncidentResponse(IncidentResponse.newBuilder().setIncidentId(resourceMapping.getResourceId()).setStatus(resourceMapping.getStatus()).build());
+        resourceOrchProducer.sendIncidentResponse(IncidentResponse.newBuilder().setIncidentId(resourceMapping.getResourceId()).setIncidentStatus(Common.IncidentStatus.valueOf(resourceMapping.getStatus())).build());
 
     }
 
