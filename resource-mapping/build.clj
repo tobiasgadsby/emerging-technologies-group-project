c989e447ae3cd0935ace866cc717e679fb7b7fa2
James Crake-Merani <james@jamescm.co.uk>
2026-04-15 20:43:30 +0100

    Extend build file so the project is installable.

--- resource-mapping/build.clj
+++ resource-mapping/build.clj
@@ -7,6 +7,9 @@
 (def main 'jamescrake-merani.resource-mapping)
 (def class-dir "target/classes")
 
+(defn clean "Delete the build target directory." [_]
+  (b/delete {:path "target"}))
+
 (defn test "Run all the tests." [opts]
   (let [basis    (b/create-basis {:aliases [:test]})
         cmds     (b/java-command
@@ -20,20 +23,45 @@
 (defn- uber-opts [opts]
   (assoc opts
          :lib lib :main main
-         :uber-file (format "target/%s-%s.jar" lib version)
+         :uber-file (format "target/%s-%s-standalone.jar" (name lib) version)
          :basis (b/create-basis {})
          :class-dir class-dir
          :src-dirs ["src"]
          :ns-compile [main]))
 
+(defn jar "Build the library JAR file." [opts]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis (b/create-basis {})
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (let [jar-file (format "target/%s-%s.jar" (name lib) version)]
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})
+    (println "Built" jar-file)
+    (assoc opts :jar-file jar-file)))
+
+(defn install "Install the library JAR to the local Maven repository." [opts]
+  (let [basis (b/create-basis {})
+        {:keys [jar-file]} (jar opts)]
+    (b/install {:basis basis
+                :lib lib
+                :version version
+                :jar-file jar-file
+                :class-dir class-dir})
+    (println "Installed" jar-file "to local repo")
+    opts))
+
 (defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
   (test opts)
-  (b/delete {:path "target"})
+  (clean opts)
   (let [opts (uber-opts opts)]
     (println "\nCopying source...")
     (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
     (println (str "\nCompiling " main "..."))
     (b/compile-clj opts)
-    (println "\nBuilding JAR..." (:uber-file opts))
+    (println "\nBuilding Uberjar..." (:uber-file opts))
     (b/uber opts))
   opts)
