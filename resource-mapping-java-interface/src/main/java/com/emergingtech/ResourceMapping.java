d6517251cd30b163c3e815177e768df47e4d62ee
James Crake-Merani <james@jamescm.co.uk>
2026-04-17 18:42:30 +0100

    Return a proto response for dispatch.

--- resource-mapping-java-interface/src/main/java/com/emergingtech/ResourceMapping.java
+++ resource-mapping-java-interface/src/main/java/com/emergingtech/ResourceMapping.java
@@ -4,6 +4,11 @@ import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 import clojure.lang.PersistentArrayMap;
 
+import java.time.Instant;
+
+import com.emergingtech.proto.ResourceMapper;
+import com.google.protobuf.Timestamp;
+
 public class ResourceMapping {
     private IFn positions;
     private IFn graph;
@@ -13,11 +18,18 @@ public class ResourceMapping {
         positions = Clojure.var("jamescrake-merani.resource-mapping", "positions");
         graph = Clojure.var("jamescrake-merani.resource-mapping", "graph");
     }
-    private void dispatchAmbulance(String patientLocation) {
+    public ResourceMapper.ResourceMappingResponse dispatchAmbulance(String patientLocation) {
         IFn keyword = Clojure.var("clojure.core", "keyword");
         Object patientLocationKeyword = keyword.invoke(patientLocation);
         IFn dispatch = Clojure.var("jamescrake-merani.resource-mapping", "dispatch");
         PersistentArrayMap dispatchResult = (PersistentArrayMap)dispatch.invoke(graph, positions, patientLocationKeyword);
-        // TODO: Add return.
+        int etaMinutes = (int)dispatchResult.get("eta");
+        Instant etaInst = Instant.now().plusSeconds(etaMinutes * 60);
+        Timestamp etaProtoTimestamp = Timestamp.newBuilder().setSeconds(etaInst.getEpochSecond()).setNanos(etaInst.getNano()).build();
+        ResourceMapper.ResourceMappingResponse response = ResourceMapper.ResourceMappingResponse.newBuilder()
+            .setMappingId(1)
+            .setArrivalTime(etaProtoTimestamp)
+            .build();
+        return response;
     }
 }
