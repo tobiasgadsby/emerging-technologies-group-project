e5a9ca23a4c987ae73adb97a3e4354bde3d0b9b9
Toby <133044093+tobiasgadsby@users.noreply.github.com>
2026-04-19 10:22:08 +0100

    orchestration - created initial api and proto specifications for orchestration, and implemented initial version of doctor request orchestration service allowing for the creation of incidents

--- resource-mapping-java-interface/src/main/java/com/emergingtech/ResourceMapping.java
+++ resource-mapping-java-interface/src/main/java/com/emergingtech/ResourceMapping.java
@@ -8,17 +8,38 @@ import java.time.Instant;
 
 import com.emergingtech.proto.ResourceMapper;
 import com.emergingtech.proto.Common.Coordinates;
+import com.emergingtech.proto.ResourceMapper.ResourceMappingRequest;
+import com.emergingtech.proto.ResourceMapper.ResourceMappingResponse;
 import com.google.protobuf.Timestamp;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
 
 public class ResourceMapping {
+
+    private static final Logger LOG = Logger.getLogger(ResourceMapping.class);
+
     private IFn positions;
     private IFn graph;
+
+    @Channel("resource-response")
+    Emitter<ResourceMappingResponse> emitter;
+
     public ResourceMapping() {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("jamescrake-merani.resource-mapping"));
         positions = Clojure.var("jamescrake-merani.resource-mapping", "positions");
         graph = Clojure.var("jamescrake-merani.resource-mapping", "graph");
     }
+
+    @Incoming("resource-request")
+    public void resourceRequest(ResourceMappingRequest resourceMappingRequest) {
+        LOG.infof("Resource Request Received, Mapping ID: %d", resourceMappingRequest.getMappingId());
+        emitter.send(dispatchAmbulance(resourceMappingRequest));
+    }
+
+
     public ResourceMapper.ResourceMappingResponse dispatchAmbulance(ResourceMapper.ResourceMappingRequest request) {
         Coordinates patientCoordinates =  request.getPatientCoordinates();
         int unpairedCoord = (int)(patientCoordinates.getX() + patientCoordinates.getY());
@@ -31,7 +52,7 @@ public class ResourceMapping {
         Instant etaInst = Instant.now().plusSeconds(etaMinutes * 60);
         Timestamp etaProtoTimestamp = Timestamp.newBuilder().setSeconds(etaInst.getEpochSecond()).setNanos(etaInst.getNano()).build();
         ResourceMapper.ResourceMappingResponse response = ResourceMapper.ResourceMappingResponse.newBuilder()
-            .setMappingId(1)
+            .setMappingId(request.getMappingId())
             .setArrivalTime(etaProtoTimestamp)
             .build();
         return response;
